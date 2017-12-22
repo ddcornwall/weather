9964f4dab45bcbf2cf2f196d8149143a6d3acd88
Daniel Cornwall <32757825+ddcornwall@users.noreply.github.com>
2017-12-22 04:58:15 -0900

    Revised files

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-
-
-// Try HTML5 geolocation.
+// Get user location by using HTML5 geolocation. Needs to fail gracefully
    if (navigator.geolocation) {
 navigator.geolocation.getCurrentPosition(function(position) {
 var lat=position.coords.latitude;
@@ -12,10 +10,26 @@ wURL="https://fcc-weather-api.glitch.me/api/current?lon=" +lng + "&" + "lat=" + 
 console.log(wURL);
 $.getJSON(wURL, function( data ) {
  conditions=data;
-$("#local-location").append("<p> Conditions for " + conditions.name + " located at </br>" +  "Latitude: " + conditions.coord.lat + "</br>" +
+
+//display location information
+$("#local-location").append("<p>" + conditions.name + " located at </br>" +  "Latitude: " + conditions.coord.lat + "</br>" +
 "Longitude: " + conditions.coord.lon + "</p>");
-$("#local-weather").append("<img src = \"" + conditions.weather[0].icon + "\"> </br>" + conditions.weather[0].description);
-$("#local-weather").append("</br></br>Temperature (C): " + conditions.main.temp);
+
+//display sky conditions
+$("#local-sky").append("<img src = \"" + conditions.weather[0].icon + "\"> </br>" + conditions.weather[0].description);
+
+//display weather conditions
+$("#local-weather").append("Temperature: " + conditions.main.temp + " C");
+$("#local-weather").append("</br>Pressure: " + conditions.main.pressure + " mb");
+$("#local-weather").append("</br>Humidity: " + conditions.main.humidity + " %");
+$("#local-weather").append("</br>Wind Speed: " + conditions.wind.speed +" meters per second");
+$("#local-weather").append("</br>Wind Direction: " + conditions.wind.deg + " Deg");
+
+//Calculate and display sunrise and sunset
+var sunrise =  new Date(conditions.sys.sunrise*1000);
+var sunset = new Date(conditions.sys.sunset*1000);
+$("#astronomy").append("Sunrise: " + sunrise);
+$("#astronomy").append("</br>Sunset: " + sunset);
 }); //end load weather
 });
 }
