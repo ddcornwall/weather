2c263b736ba5a1ffcb24e0a84012b81be569902a
Daniel Cornwall <32757825+ddcornwall@users.noreply.github.com>
2017-12-23 09:19:56 -0700

    Updated displays

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ $.getJSON(wURL, function( data ) {
  conditions=data;
 
 //display location information
-$("#local-location").append("<p>" + conditions.name + " located at </br>" +  "Latitude: " + conditions.coord.lat + "</br>" +
+$("#local-location").append("<p>" + conditions.name + ", located at </br>" +  "Latitude: " + conditions.coord.lat + "</br>" +
 "Longitude: " + conditions.coord.lon + "</p>");
 
 //display sky conditions
-$("#local-sky").append("<img src = \"" + conditions.weather[0].icon + "\"> </br>" + conditions.weather[0].description);
+$("#local-sky").append("<img src = \"" + conditions.weather[0].icon + "\"" + " class=\"center-block\" height=\"125\" width=\"125\" > </br>" + conditions.weather[0].description);
 
 //display weather conditions
 $("#local-weather").append("Temperature: " + conditions.main.temp + " C");
@@ -28,8 +28,11 @@ $("#local-weather").append("</br>Wind Direction: " + conditions.wind.deg + " Deg
 //Calculate and display sunrise and sunset
 var sunrise =  new Date(conditions.sys.sunrise*1000);
 var sunset = new Date(conditions.sys.sunset*1000);
-$("#astronomy").append("Sunrise: " + sunrise);
-$("#astronomy").append("</br>Sunset: " + sunset);
+var month = sunrise.getMonth() + 1; //Get month returns 0-11 for months
+$("#astronomy").append("Date: " + month + "/" + sunrise.getDate() + "/" + sunrise.getFullYear() +"</br>");
+$("#astronomy").append("Sunrise: " + sunrise.toLocaleTimeString());
+$("#astronomy").append("</br>Sunset:   " + sunset.toLocaleTimeString());
+console.log(sunrise);
 }); //end load weather
 });
 }
