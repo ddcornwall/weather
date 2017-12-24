07d966d024c39b8e0159ca64dce2f4131c469bbf
Daniel Cornwall <32757825+ddcornwall@users.noreply.github.com>
2017-12-24 07:55:42 -0700

    Fully functioning code

--- script.js
+++ script.js
@@ -19,11 +19,35 @@ $("#local-location").append("<p>" + conditions.name + ", located at </br>" +  "L
 $("#local-sky").append("<img src = \"" + conditions.weather[0].icon + "\"" + " class=\"center-block\" height=\"125\" width=\"125\" > </br>" + conditions.weather[0].description);
 
 //display weather conditions
-$("#local-weather").append("Temperature: " + conditions.main.temp + " C");
-$("#local-weather").append("</br>Pressure: " + conditions.main.pressure + " mb");
-$("#local-weather").append("</br>Humidity: " + conditions.main.humidity + " %");
-$("#local-weather").append("</br>Wind Speed: " + conditions.wind.speed +" meters per second");
-$("#local-weather").append("</br>Wind Direction: " + conditions.wind.deg + " Deg");
+var degF = 9*conditions.main.temp/5+32; // calculate temp in fahrenheit
+$("#local-weather").append("Temperature: " + conditions.main.temp.toFixed(0) + "C / " + degF.toFixed(0) + "F");
+     // caluculate pressure in inches
+var inches = 0.0295300*conditions.main.pressure;
+$("#local-weather").append("</br>Pressure: " + conditions.main.pressure.toFixed(1) + "mb  / " + inches.toFixed(1) + "in");
+$("#local-weather").append("</br>Humidity: " + conditions.main.humidity + "%");
+     // calculate windspeed in miles per hour
+var mph = (conditions.wind.speed*3600)/1609.34;
+$("#local-weather").append("</br>Wind Speed: " + conditions.wind.speed.toFixed(1) +"mps / " + mph.toFixed(1) +"mph");
+    //convert degrees to a wind Direction
+    var winDir="";
+    if (conditions.wind.deg >= 0 && conditions.wind.deg <= 33.75) {
+    winDir="N";
+  } else if (conditions.wind.deg >= 33.76 && conditions.wind.deg <= 78.75) {
+    winDir="NE";
+  } else if (conditions.wind.deg >= 78.76 && conditions.wind.deg <= 123.75) {
+    winDir="E";
+  } else if (conditions.wind.deg >= 123.76 && conditions.wind.deg <= 168.75) {
+    winDir="SE";
+  } else if (conditions.wind.deg >= 168.76 && conditions.wind.deg <= 213.75) {
+    winDir="S";
+    } else if (conditions.wind.deg >= 213.76 && conditions.wind.deg <= 258.75) {
+    winDir="SW";
+    } else if (conditions.wind.deg >= 258.76 && conditions.wind.deg <= 303.75) {
+    winDir="W";
+  } else if (conditions.wind.deg >= 303.76 && conditions.wind.deg <= 359.99) {
+    winDir="NW";
+    }
+  $("#local-weather").append("</br>Wind Direction: " + winDir);
 
 //Calculate and display sunrise and sunset
 var sunrise =  new Date(conditions.sys.sunrise*1000);
